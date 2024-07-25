9faf73c943d910e84262e6205e451004684f4c06
Joshua Roberts <joshua.roberts@securepoint.de>
2024-07-25 13:54:52 +0200

    Fix: added Linux Support site

---
+++ resources/views/Sites/LinuxSupport.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.common')
+@section('content')
+<div id="app" class="w-100">
+    <h3>Du hast keine Lust, Windows 11 zu nutzen?</h3>
+    <p>Aber gleichzeitig auch keine lust, auf
+        <a href="https://www.computerbild.de/artikel/cb-Tipps-Software-Windows-10-wird-eingestellt-Keine-Updates-mehr-was-tun-36482825.html"
+           style="text-decoration-color: #0dcaf0; color: #0dcaf0;"
+           rel="noopener noreferrer" target="_blank">
+            den Support von Windows 10 zu verzichten</a>?
+    </p>
+    <p>Schreib mir eine Mail an <span class="link" style="color: #0dcaf0">linux-support@die-enten-im-park-sind.gratis</span>!</p>
+</div>
+@endsection
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,6 +20,9 @@
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ms-auto">
                 <!-- Authentication Links -->
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('linux') }}">Linux Support</a>
+                </li>
                 @guest
                     @if (Route::has('login'))
                         <li class="nav-item">
