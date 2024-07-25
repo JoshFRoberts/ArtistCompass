72aaf61c62db16dcb96ad846a9505f650b9665d5
Joshua <joshua.roberts@gmx.de>
2024-07-25 19:44:29 +0200

    updates linux support page with better text

--- resources/views/Sites/LinuxSupport.blade.php
+++ resources/views/Sites/LinuxSupport.blade.php
@@ -6,8 +6,11 @@
         <a href="https://www.computerbild.de/artikel/cb-Tipps-Software-Windows-10-wird-eingestellt-Keine-Updates-mehr-was-tun-36482825.html"
            style="text-decoration-color: #0dcaf0; color: #0dcaf0;"
            rel="noopener noreferrer" target="_blank">
-            den Support von Windows 10 zu verzichten</a>?
+            den Support von Windows 10 zu verzichten</a>,
+        wenn dieser im Oktober 2025 eingestellt wird?
     </p>
-    <p>Schreib mir eine Mail an <span class="link" style="color: #0dcaf0">linux-support@die-enten-im-park-sind.gratis</span>!</p>
+    <p>Mac ist dir zu teuer?</p>
+    <p>Und mit Linux kennst du dich nicht gut genug aus, um den Umzug "Einfach so" zu machen?</p>
+    <p>Schreib mir eine Mail an <span class="link" style="color: #0dcaf0">joshua.roberts@gmx.de</span>!</p>
 </div>
 @endsection
